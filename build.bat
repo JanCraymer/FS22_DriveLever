1110b9ab6e27c0b8278cab8c6486a70f61bd4f5e
Jan <26329438+JanCraymer@users.noreply.github.com>
2022-01-21 18:09:04 +0100

    change build script

---
+++ build.bat
@@ -0,0 +1,2 @@
+del FS22_drivingStick.zip
+"%programfiles%\7-Zip\7z.exe" a -tzip FS22_drivingStick.zip modDesc.xml icon.dds src/*
--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-zip FS22_drivingStick.zip src/* icon.dds modDesc.xml

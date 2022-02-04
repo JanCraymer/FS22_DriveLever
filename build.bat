dcdcf0e1015ff3bc8f63371d8ceec2a2df89983b
Jan <26329438+JanCraymer@users.noreply.github.com>
2022-02-04 10:33:36 +0100

    rename to drive lever

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-del FS22_drivingStick.zip
-"%programfiles%\7-Zip\7z.exe" a -tzip FS22_drivingStick.zip modDesc.xml icon.dds src/*
+del FS22_DriveLever.zip
+"%programfiles%\7-Zip\7z.exe" a -tzip FS22_DriveLever.zip modDesc.xml icon.dds src/*
 pause

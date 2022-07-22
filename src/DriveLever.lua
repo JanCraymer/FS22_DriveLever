910e4898487368e1028d1ce44cdd841f02671947
Jan <26329438+JanCraymer@users.noreply.github.com>
2022-07-23 00:35:42 +0200

    always use real vehicle speed for calculation of new speed

--- src/DriveLever.lua
+++ src/DriveLever.lua
@@ -220,7 +220,7 @@ function DriveLever:onUpdate(dt)
                                     newSpeed = math.floor(lastSpeed)
                                 else
                                     local speedChange = calculateSpeedChangeStep(spec.input.forward.value)
-                                    newSpeed = spec.vehicle.targetSpeed + speedChange
+                                    newSpeed = math.floor(lastSpeed) + speedChange
                                 end
 
                                 if newSpeed - lastSpeed > spec.vehicle.maxSpeedAdvance then
@@ -238,7 +238,7 @@ function DriveLever:onUpdate(dt)
                                 newSpeed = math.floor(lastSpeed)
                             else
                                 local speedChange = calculateSpeedChangeStep(spec.input.backward.value)
-                                newSpeed = spec.vehicle.targetSpeed - speedChange
+                                newSpeed = math.floor(lastSpeed) - speedChange
                             end
 
                             if newSpeed > lastSpeed then
